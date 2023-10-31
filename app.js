d4baaa5d3f04ba9976793314f495c278be1a5000
Hugo Gabriel Caamaño <gabrielc83@mail.com>
2023-10-31 16:48:33 -0300

    Probando cambios en el standard. Pasamos de commonjs a module

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require("express");
+import express from "express";
+
 const indexRoutes = require("./src/routes/index.routes");
 const userRoutes = require("./src/routes/users.routes");
 const productsRoutes = require("./src/routes/products.routes");
