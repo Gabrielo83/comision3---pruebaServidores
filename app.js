1b0f54db6af9412f5a56c3f82fb94afcc1b77e32
Hugo Gabriel Caamaño <gabrielc83@mail.com>
2023-10-31 16:52:16 -0300

    Se crea el proyecto, se añaden rutas y controladores de usuario, productos y el index

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-import express from "express";
-
+const express = require("express");
 const indexRoutes = require("./src/routes/index.routes");
 const userRoutes = require("./src/routes/users.routes");
 const productsRoutes = require("./src/routes/products.routes");
